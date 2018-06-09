7b289c3671367390a9d8d0f078777d56a56faa21
Szymon Grabowski <szmiko@wp.pl>
2018-06-09 19:46:22 +0200

    Script2

--- script2.js
+++ script2.js
@@ -1,7 +1,6 @@
 let a = prompt('Podaj liczbę a');
 let b = prompt('podaj liczbę b');
 
-let multiply = (x, y) => (a * (b || 1));
-
+let multiply = (x, y = 10) => (a * b);
 
 console.log(multiply(a, b));
